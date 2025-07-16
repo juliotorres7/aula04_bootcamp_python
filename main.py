d06df2e107bb587ccb928188ad1100f8d4b38c2e
Julio Torres <julio.torres7@outlook.com>
2025-07-16 19:35:19 -0300

    adicionando os exerciocios com o primeiro resolvido

---
+++ main.py
@@ -0,0 +1,39 @@
+# Exercícios de Listas e Dicionários
+
+# 1 Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+
+lista_numeros = []
+
+for i in range(11):
+    lista_numeros.append(i)
+
+for i in lista_numeros:
+    print(i)
+
+# 2 Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+
+
+# 3 Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de 
+# publicação. Imprima cada informação.
+
+
+# 4 Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
+
+
+# 5 Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65},
+# calcule o preço total da lista de compras.
+
+
+# Exercícios de Funções
+# 16 Escreva uma função que receba uma lista de números e retorne a soma de todos os números.
+# 
+# 17 Crie uma função que receba um número como argumento e retorne True se o número 
+# for primo e False caso contrário.
+# 
+# 18 Desenvolva uma função que receba uma string como argumento e retorne essa string revertida.
+# 
+# 19 Implemente uma função que receba dois argumentos: uma lista de números e um número. 
+# A função deve retornar todas as combinações de pares na lista que somem ao número dado.
+# 
+# 20 Escreva uma função que receba um dicionário e retorne uma lista de chaves ordenadas
+
